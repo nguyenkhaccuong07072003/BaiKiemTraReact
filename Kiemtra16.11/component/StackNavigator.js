638a1a9b8fac76ffbabb0a2d03078614bc30689f
nguyenkhaccuong07072003 <124782853+nguyenkhaccuong07072003@users.noreply.github.com>
2023-11-16 11:39:40 +0700

    init

--- Kiemtra16.11/component/StackNavigator.js
+++ Kiemtra16.11/component/StackNavigator.js
@@ -30,28 +30,12 @@ const StackNavigation = ({ props }) => {
     <NavigationContainer>
       <Stack.Navigator
         initialRouteName="BottomTabs" screenOptions={{ headerShown: false }}>
-        {/* <Stack.Screen name="Welcome" component={Welcome} /> */}
-        <Stack.Screen name="Start" component={Start} />
         <Stack.Screen name="BottomTabs" component={BottomTabs} />
-        <Stack.Screen name="EditProfile" component={EditProfile} />
-        <Stack.Screen name="Notification" component={Notification} />
-        <Stack.Screen name="Security" component={Security} />
-        <Stack.Screen name="ViewTicket" component={ViewTicket} />
+
         <Stack.Screen name="Booking" component={Booking} />
         <Stack.Screen name="Scan" component={Scan} />
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Signup" component={Signup} />
-        {/* <Stack.Screen name="FillProfile" component={FillProfile} /> */}
-        <Stack.Screen name="Forgotpassword" component={Forgotpassword} />
-        <Stack.Screen name="Verify" component={Verify} />
-        <Stack.Screen name="NewPassword" component={NewPassword} />
-        <Stack.Screen name="PaymentMethod" component={PaymentMethod} />
-        <Stack.Screen name="NewCard" component={NewCard} />
-        <Stack.Screen name="Recently" component={Recently} />
-        <Stack.Screen name="Reservation" component={Reservation} />
-        <Stack.Screen name="Detailbooking" component={Detailbooking} />
-        <Stack.Screen name="Date" component={Date} />
-        <Stack.Screen name="HomeScreen" component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   )
